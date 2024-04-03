028a8b3f732b060147d508e105dd9908e6bf86bc
Lance <dogmandevices@gmail.com>
2024-04-03 18:53:38 -0500

    remove unneeded map class, delete commented out code that has been moved to player class

--- main.lua
+++ main.lua
@@ -4,56 +4,7 @@ function love.load()
 	sti = require 'lib/sti/sti'
 	map = sti("assets/desert.lua")
 	require "player"
-		
-	-- layer = map:addCustomLayer("Sprites", 3)
-	
-	-- for k, object in pairs(map.objects) do
-	-- 	if object.name == "Player" then
-	-- 		playerOnMap = object
-	-- 		break
-	-- 	end
-	-- end
 	player = Player()
-
-
-	-- local sprite = love.graphics.newImage('assets/test.png')
-	-- layer.player = {
-	-- 	sprite = sprite,
-	-- 	x = player.x,
-	-- 	y = player.y,
-	-- 	ox = sprite:getWidth()/2,
-	-- 	oy = sprite:getHeight()/1.35
-	-- }
-
-	-- layer.update = function(self, dt)
-	-- 	local speed = 96 * dt
-	-- 	if love.keyboard.isDown("w", "up") then
-	-- 		self.player.y = self.player.y - speed
-	-- 	end
-	-- 	if love.keyboard.isDown("s", "down") then
-	-- 		self.player.y = self.player.y + speed
-	-- 	end
-	-- 	if love.keyboard.isDown("a", "left") then
-	-- 		self.player.x = self.player.x - speed
-	-- 	end
-	-- 	if love.keyboard.isDown("d", "right") then
-	-- 		self.player.x = self.player.x + speed
-	-- 	end
-	-- end
-
-	-- layer.draw = function(self)
-		-- love.graphics.draw(
-		-- 	self.player.sprite,
-		-- 	math.floor(self.player.x),
-		-- 	math.floor(self.player.y),
-		-- 	0,
-		-- 	1,
-		-- 	1,
-		-- 	self.player.ox,
-		-- 	self.player.oy
-		-- )
-		
-	-- end
 end
 
 function love.update(dt)
