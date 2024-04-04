73029bd3775f928d61c2a31825e5f6ddd8eca255
Lance <dogmandevices@gmail.com>
2024-04-03 19:49:14 -0500

    add title to window, disable unused modules, include LOVE version

--- conf.lua
+++ conf.lua
@@ -1,4 +1,8 @@
 function love.conf(t)
 	t.window.width = 800
 	t.window.height = 800
+	t.modules.joystick = false
+	t.modules.physics = false
+	t.version = "11.5"
+	t.window.title = "Endless Desert"
 end
