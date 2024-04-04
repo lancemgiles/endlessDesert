bd1df5cde3e55f0cc5058994ca44fb0922c9ee30
Lance <dogmandevices@gmail.com>
2024-04-03 19:25:43 -0500

    make map wrap around itself, replace test sprite with animated sprite sheet

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 function love.load()
 	Object = require "lib/classic"
 	windowSize = love.window.setMode(800, 800)
+	mapWidth = 800
 	sti = require 'lib/sti/sti'
 	map = sti("assets/desert.lua")
 	require "player"
@@ -16,6 +17,24 @@ function love.draw()
 	local scale = 8
 	local screen_width = love.graphics.getWidth() / scale
 	local screen_height = love.graphics.getHeight() / scale
-	map:draw(0, 0, scale)
+
+	local camera = map.layers["Sprites"].player
+	local cx = math.floor(player.x - screen_width / 2)
+	local cy = math.floor(player.y - screen_height / 2)
+	map:draw(-cx, -cy, scale)
 	player:draw()
+	mapLoop()
+end
+
+function mapLoop()
+	if player.x < 100 then
+		player.x = mapWidth - 100
+	elseif player.x > mapWidth - 100 then
+		player.x = 100
+	end
+	if player.y > mapWidth -100 then
+		player.y = 100
+	elseif player.y < 100 then
+		player.y = mapWidth - 100
+	end
 end
--- player.lua
+++ player.lua
@@ -1,7 +1,7 @@
 Player = Object:extend()
 
 function Player:new()
-	self.sprite = love.graphics.newImage('assets/test.png')
+	-- self.sprite = love.graphics.newImage('assets/test.png')
 	local layer = map:addCustomLayer("Sprites", 3)
 	local spawn
 	for k, object in pairs(map.objects) do
@@ -14,52 +14,45 @@ function Player:new()
 	self.x = spawn.x
 	print(self.x)
 	self.y = spawn.y
-	-- self.ox = self.sprite:getWidth()/2
-	-- self.oy = self.sprite:getHeight()/1.35
-	-- self.spriteSheet = love.graphics.newImage("assets/sandmanSheet")
-	-- self.width = spriteSheet:getWidth()
-	-- self.height = spriteSheet:getHeight()
-	-- self.frames = {}
-	-- self.frame_width = 16
-	-- self.frame_height = 16
-	-- for i=0, 3 do
-	-- 	table.insert(self.frames, love.graphics.newQuad(i * self.frame_width, 0,
-	-- 		self.frame_width, self.frame_height, self.width, self.height))
-	-- end
-	-- currentFrame = 1
-	
-	
 
-
+	self.spriteSheet = love.graphics.newImage("assets/sandmanSheet.png")
+	self.width = self.spriteSheet:getWidth()
+	self.height = self.spriteSheet:getHeight()
+	self.frames = {}
+	self.frame_width = 16
+	self.frame_height = 16
+	for i=0, 3 do
+		table.insert(self.frames, love.graphics.newQuad(i * self.frame_width, 0,
+			self.frame_width, self.frame_height, self.width, self.height))
+	end
+	self.currentFrame = 1
+	self.sprite = self.frames[self.currentFrame]
 end
 
--- sprite sheet: down 1, up 2, left 3, right 4
 
 function Player:update(dt)
+	-- sprite sheet: down 1, up 2, left 3, right 4
 	speed = 96 * dt
 		if love.keyboard.isDown("w", "up") then
 			self.y = self.y - speed
-		end
-		if love.keyboard.isDown("s", "down") then
+			self.currentFrame = 2
+		elseif love.keyboard.isDown("s", "down") then
 			self.y = self.y + speed
+			self.currentFrame = 1
 		end
 		if love.keyboard.isDown("a", "left") then
 			self.x = self.x - speed
-		end
-		if love.keyboard.isDown("d", "right") then
+			self.currentFrame = 4
+		elseif love.keyboard.isDown("d", "right") then
 			self.x = self.x + speed
+			self.currentFrame = 3
 		end
 end
 
 function Player:draw()
-	love.graphics.draw(self.sprite,
+	love.graphics.draw(self.spriteSheet, self.frames[self.currentFrame],
 			self.x,
 			self.y
-			-- 0,
-			-- 1,
-			-- 1,
-			-- self.ox,
-			-- self.oy
 		)
 end
 
